d99b05fb84e12577849cd207ef0bec9a0433ed3f
millJosh <113854085+millJosh@users.noreply.github.com>
2023-10-02 19:13:57 -0400

    Create Lab5.py
    Initial Commit

---
+++ labs/Lab5.py
@@ -0,0 +1,20 @@
+import random
+
+random_number = random.randint(1, 30)
+
+print("Guess Number Game!")
+print("Guess a number between 1 and 30. Try your best to guess it.")
+
+attempts = 0
+
+while True:
+	user_guess = int(input("Enter a guess: "))
+	attempts += 1
+
+	if user_guess < random_number:
+		print("Too Low, Try again")
+	elif user_guess > random_number:
+		print("Too High, Try again")
+	else:
+		print("Good Job, You guessed the number {} in {} attempts.".format(random_number, attempts))
+		break
