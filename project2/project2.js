a4b01dffe8aa31bef6937e18ef6fe21423cf8e8e
millJosh <113854085+millJosh@users.noreply.github.com>
2023-10-31 22:25:14 -0400

    Update project2.js

--- project2/project2.js
+++ project2/project2.js
@@ -1,1 +1,16 @@
+const os = require('os');
+const fs = require('fs');
 
+function bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Byte';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
+const totalSpace = os.totalmem();
+const freeSpace = os.freemem();
+
+console.log('Total Space:', bytesToSize(totalSpace));
+console.log('Free Space:', bytesToSize(freeSpace));
+console.log('Used Space:', bytesToSize(totalSpace - freeSpace));
