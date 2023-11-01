48fc0e73199d074c071f01bac5362cdee1e24f16
millJosh <113854085+millJosh@users.noreply.github.com>
2023-11-01 00:10:03 -0400

    Update project2.js

--- project2/project2.js
+++ project2/project2.js
@@ -14,3 +14,29 @@ const totalMemory = os.totalmem();
 console.log('Total Memory:', bytesToSize(totalMemory));
 console.log('Available Memory:', bytesToSize(availableMemory));
 console.log('Used Memory:', bytesToSize(totalMemory - availableMemory));
+
+function saveMemoryInfoToFile() {
+    const availableMemory = os.freemem();
+    const totalMemory = os.totalmem();
+
+    const memoryInfo = `
+        Total Memory: ${bytesToSize(totalMemory)}
+        Available Memory: ${bytesToSize(availableMemory)}
+        Used Memory: ${bytesToSize(totalMemory - availableMemory)}
+    `;
+
+    fs.writeFile('memory_info.txt', memoryInfo, (err) => {
+        if (err) {
+            console.error('Error writing memory info to file:', err);
+        } else {
+            console.log('Memory info saved to memory_info.txt');
+        }
+    });
+}
+
+setInterval(saveMemoryInfoToFile, 10000);
+
+const usedMemoryPercentage = ((totalMemory - availableMemory) / totalMemory) * 100;
+console.log('Used Memory Percentage:', usedMemoryPercentage.toFixed(2) + '%');
+
+
