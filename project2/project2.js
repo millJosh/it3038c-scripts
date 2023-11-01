7bc1ce51be4a201d30aaf152ea79b2fa975bd5a4
millJosh <113854085+millJosh@users.noreply.github.com>
2023-11-01 00:55:27 -0400

    Update project2.js

--- project2/project2.js
+++ project2/project2.js
@@ -1,7 +1,8 @@
+//First Part: Telling someone how much memory they have
 const os = require('os');
 const fs = require('fs');
 
-function bytesToSize(bytes) {
+function datasize(bytes) {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     if (bytes === 0) return '0 Byte';
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
@@ -11,32 +12,43 @@ function bytesToSize(bytes) {
 const availableMemory = os.freemem();
 const totalMemory = os.totalmem();
 
-console.log('Total Memory:', bytesToSize(totalMemory));
-console.log('Available Memory:', bytesToSize(availableMemory));
-console.log('Used Memory:', bytesToSize(totalMemory - availableMemory));
+console.log('Total Memory:', datasize(totalMemory));
+console.log('Available Memory:', datasize(availableMemory));
+console.log('Used Memory:', datasize(totalMemory - availableMemory));
 
-function saveMemoryInfoToFile() {
-    const availableMemory = os.freemem();
-    const totalMemory = os.totalmem();
+//Second Part: Converting Memory to a Percentage
 
-    const memoryInfo = `
-        Total Memory: ${bytesToSize(totalMemory)}
-        Available Memory: ${bytesToSize(availableMemory)}
-        Used Memory: ${bytesToSize(totalMemory - availableMemory)}
+const UPercentage = ((totalMemory - availableMemory) / totalMemory) * 100;
+const APercentage = (availableMemory / totalMemory) * 100;
+console.log('Avilable Memory Percentage:', APercentage.toFixed(2) + '%');
+console.log('Used Memory Percentage:', UPercentage.toFixed(2) + '%');
+
+//Third Part: Saving memory information to a file
+
+let updatesCount = 0;
+
+function Filesave() {
+
+    const DataInfo = `
+        Total Memory: ${datasize(totalMemory)}
+        Available Memory: ${datasize(availableMemory)}
+        Used Memory: ${datasize(totalMemory - availableMemory)}
+	Used Memory Percentage: ${(UPercentage)}
+	Avilable Memory Percentage: ${(APercentage)}
     `;
 
-    fs.writeFile('memory_info.txt', memoryInfo, (err) => {
+    fs.writeFile('MemoryData.txt', DataInfo, (err) => {
         if (err) {
-            console.error('Error writing memory info to file:', err);
+            console.error('Error writing memory file', err);
         } else {
-            console.log('Memory info saved to memory_info.txt');
+            console.log('Memory data saved to MemoryData.txt');
+	    updatesCount++;
+
+	    if (updatesCount >= 3) {
+		clearInterval(interval);
+		}
         }
     });
 }
 
-setInterval(saveMemoryInfoToFile, 10000);
-
-const usedMemoryPercentage = ((totalMemory - availableMemory) / totalMemory) * 100;
-console.log('Used Memory Percentage:', usedMemoryPercentage.toFixed(2) + '%');
-
-
+const interval = setInterval(Filesave, 10000);
