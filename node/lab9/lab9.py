82e2441fda1c6121c9cc804c6a81c047648853ab
millJosh <113854085+millJosh@users.noreply.github.com>
2023-11-05 00:09:43 -0400

    Update lab9.py

--- node/lab9/lab9.py
+++ node/lab9/lab9.py
@@ -5,9 +5,9 @@ api_url = "http://localhost:3000"
 response = requests.get(api_url)
 
 if response.status_code == 200:
-    widget_data = response.json()
-    if widget_data:
-        for index, widget in enumerate(widget_data, start=1):
+    widget = response.json()
+    if widget:
+        for index, widget in enumerate(widget, start=1):
             widget_name = widget.get("name", "Unknown")
             widget_color = widget.get("color", "Unknown")
             print("Widget{} is {}.".format(index, widget_color))
@@ -15,3 +15,4 @@ if response.status_code == 200:
         print("No widget data found.")
 else:
     print("Failed to retrieve data from the API. Status code: {}".format(response.status_code))
+
